6a32b47fd443b3b63d6842ce1e4295e64b55c44b
Altamir Cassiano <altamirdiascassiano@hotmail.com>
2020-12-20 20:25:33 -0300

    First commit

---
+++ SimpleObserverPatternInCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleObserverPatternInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
